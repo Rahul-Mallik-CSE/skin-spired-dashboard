595c55a961265f2802b6b0afab94c4192e2d2c21
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2025-11-22 09:53:25 +0600

    style: format code by adding missing semicolons and formatting comments

--- hooks/use-mobile.tsx
+++ hooks/use-mobile.tsx
@@ -1,26 +1,28 @@
-"use client"
+/** @format */
 
-import { useEffect, useState } from "react"
+"use client";
+
+import { useEffect, useState } from "react";
 
 export function useIsMobile() {
-  const [isMobile, setIsMobile] = useState(false)
+  const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
     const checkIsMobile = () => {
-      setIsMobile(window.innerWidth < 768)
-    }
+      setIsMobile(window.innerWidth < 768);
+    };
 
-    // Initial check
-    checkIsMobile()
+    // Initial check ..
+    checkIsMobile();
 
     // Add event listener
-    window.addEventListener("resize", checkIsMobile)
+    window.addEventListener("resize", checkIsMobile);
 
     // Clean up
     return () => {
-      window.removeEventListener("resize", checkIsMobile)
-    }
-  }, [])
+      window.removeEventListener("resize", checkIsMobile);
+    };
+  }, []);
 
-  return isMobile
+  return isMobile;
 }
